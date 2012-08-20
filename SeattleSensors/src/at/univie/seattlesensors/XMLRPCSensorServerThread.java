a0b0c1b215b68a9d173e58163fc69a7734ebd577
Florian Metzger <florian.metzger@univie.ac.at>
2012-08-20 10:29:44 +0200

    added license headers

--- SeattleSensors/src/at/univie/seattlesensors/XMLRPCSensorServerThread.java
+++ SeattleSensors/src/at/univie/seattlesensors/XMLRPCSensorServerThread.java
@@ -1,3 +1,25 @@
+/*
+ *  * Copyright (C) 2012 Florian Metzger
+ * 
+ *  This file is part of android-seattle-sensors.
+ *
+ *   Foobar is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Foobar is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with android-seattle-sensors. If not, see
+ *   <http://www.gnu.org/licenses/>.
+ * 
+ * 
+ */
+
 package at.univie.seattlesensors;
 
 import java.net.InetAddress;
