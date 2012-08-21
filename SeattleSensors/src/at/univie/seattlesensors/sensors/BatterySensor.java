de1241e9be57b3eaea8c2d1bf913caa4ff68693b
Florian Metzger <florian.metzger@univie.ac.at>
2012-08-21 14:50:39 +0200

    Battery sensor

---
+++ SeattleSensors/src/at/univie/seattlesensors/sensors/BatterySensor.java
@@ -0,0 +1,67 @@
+package at.univie.seattlesensors.sensors;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import at.univie.seattlesensors.SensorRegistry;
+
+public class BatterySensor extends AbstractSensor {
+	
+	private BroadcastReceiver batteryReceiver;
+	
+	private int rawlevel = -1;
+	private int level = -1;
+	private int scale = -1;
+	private int plugged = -1;
+	private int temperature = -1;
+	private double voltage = -1;
+	private String technology = "";
+
+	public BatterySensor(Context context) {
+		super(context);
+		name = "Battery Sensor";
+		enable();
+	}
+
+	@Override
+	public void enable() {
+        batteryReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                
+            	plugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+                rawlevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+                scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+                technology= intent.getExtras().getString(BatteryManager.EXTRA_TECHNOLOGY);
+                temperature= intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE,0);
+                voltage= intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE,0);
+                if (rawlevel >= 0 && scale > 0) {
+                    level = (rawlevel * 100) / scale;
+                }
+                SensorRegistry.getInstance().debugOut("Battery Level: " + level + "%");
+            }
+        };
+        IntentFilter batteryLevelFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        context.registerReceiver(batteryReceiver, batteryLevelFilter);
+        
+        super.enable();
+	}
+
+	@Override
+	public void disable() {
+		context.unregisterReceiver(batteryReceiver);
+		super.disable();
+	}
+	
+	@XMLRPCMethod
+	public Object [] batteryVoltage(){
+		return new Object [] {voltage};
+	}
+	
+	@XMLRPCMethod
+	public Object[] batteryInformation(){
+		return new Object[]{"technology", technology, "voltage", voltage, "temperature", temperature, "plugged", plugged, "rawlevel", rawlevel, "scale", scale, "level", level};
+	}
+
+}
