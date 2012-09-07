61439df573de0513e872b9c745744a57fc7fe53e
Yanyan Zhuang <yanyanzhuang83@gmail.com>
2012-09-06 20:19:01 -0700

    added getBondedDev, getScannedDev, etc

--- SeattleSensors/src/at/univie/seattlesensors/sensors/BluetoothSensor.java
+++ SeattleSensors/src/at/univie/seattlesensors/sensors/BluetoothSensor.java
@@ -1,5 +1,7 @@
 package at.univie.seattlesensors.sensors;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 import android.bluetooth.BluetoothAdapter;
@@ -18,10 +20,16 @@ public class BluetoothSensor extends AbstractSensor {
 	BroadcastReceiver bluetoothReceiver;
 	String bluetooth = "";
 	String devices = "";
+	String localDeviceName;
+	String localMAC;
+	List<Device> bondedDevices;
+	List<Device> scannedDevices;
 
 	public BluetoothSensor(Context context) {
 		super(context);
 		name = "Bluetooth Sensor";
+		bondedDevices = new LinkedList<Device>();
+		scannedDevices = new LinkedList<Device>();
 	}
 	
 	@Override
@@ -30,12 +38,15 @@ public class BluetoothSensor extends AbstractSensor {
 		bluetoothAdapter =  BluetoothAdapter.getDefaultAdapter();
 		bluetooth = "Local device\n\tName: " + bluetoothAdapter.getName() + "\n\tAddress: " 
 				+ bluetoothAdapter.getAddress();
+		localDeviceName = bluetoothAdapter.getName(); 
+		localMAC = bluetoothAdapter.getAddress();
 		
 		Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
 		bluetooth += "\nBonded devices: ";
 		if (pairedDevices.size() > 0) {
 		    for (BluetoothDevice device : pairedDevices) {
 		    	bluetooth += device.getName() + "\t" + device.getAddress();
+		    	bondedDevices.add(new Device(device.getName(), device.getAddress()));
 		    }
 		}
 		else bluetooth += "None\n";
@@ -50,15 +61,16 @@ public class BluetoothSensor extends AbstractSensor {
 						BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 						String name = device.getName();
 						if (devices.contains(name) == false) {
-							short rssi_min = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE);
+							short rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE);
 							devices += "\nDiscovered: " + name + "\tMAC address: " + device.getAddress() 
-									+ "\tRSSI: " + String.valueOf(rssi_min);
-							Log.d("Bluetooth FOUND", devices);		            	
+									+ "\tRSSI: " + String.valueOf(rssi);
+							//Log.d("Bluetooth FOUND", devices);
+							scannedDevices.add(new Device(name, device.getAddress(), rssi));
 						}
 					}
 					else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
 						SensorRegistry.getInstance().log("Bluetooth", bluetooth + devices);
-						Log.d("Bluetooth FINISHED", "done");
+						//Log.d("Bluetooth FINISHED", "done");
 						devices = "";
 						bluetoothAdapter.startDiscovery();
 					}
@@ -79,9 +91,71 @@ public class BluetoothSensor extends AbstractSensor {
 	}
 	
 	@XMLRPCMethod
-	public Object[] wifiInformation() {
+	public String getLocalDeviceName() {
+		return localDeviceName;
+	}
+	
+	@XMLRPCMethod
+	public String getLocalMAC() {
+		return localMAC;
+	}
+	
+	@XMLRPCMethod
+	public List<Device> getBondedDev(){
+		return bondedDevices;
+	}
+	
+	@XMLRPCMethod
+	public List<Device> getScannedDev(){
+		if (scannedDevices.isEmpty()) return null;
+		else return scannedDevices;
+	}
+	
+	@XMLRPCMethod
+	public Object[] bluetoothInformation() {
 		if (devices != "")
 			return new Object[] {"Bluetooth", bluetooth, "Discovered", devices};
 		return null;
+	}
+	
+	@XMLRPCMethod
+	public int batteryVoltage(){
+		return 0;
+	}
+	
+	public class Device{
+		String devName;
+		String MACAddr;
+		int RSSI;
+		
+		public Device() {
+			this.devName = null;
+			this.MACAddr = null;
+			this.RSSI = 9999; // impossible value for RSSI
+		}
+		
+		public Device(String devName, String MAC){
+			this.devName = devName;
+			this.MACAddr = MAC;
+			this.RSSI = 9999; // impossible value for RSSI
+		}
+		
+		public Device(String devName, String MAC, int rssi){
+			this.devName = devName;
+			this.MACAddr = MAC;
+			this.RSSI =rssi;
+		}
+		
+		public String getDevName(){
+			return devName;
+		}
+		
+		public String getMAC(){
+			return MACAddr;
+		}
+		
+		public int getRSSI(){
+			return RSSI;
+		}
 	}
 }
