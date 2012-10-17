85ca39c25409f62f6267c14bebec7f8b47df80bc
Yanyan Zhuang <yanyanzhuang83@gmail.com>
2012-10-17 00:03:23 -0700

    fixed wifi list growing #2

--- Sensorium/src/at/univie/sensorium/sensors/WifiSensor.java
+++ Sensorium/src/at/univie/sensorium/sensors/WifiSensor.java
@@ -10,35 +10,28 @@ import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Handler;
-import android.util.Log;
 import at.univie.sensorium.SensorRegistry;
 
 public class WifiSensor extends AbstractSensor {
 	
 	public static BroadcastReceiver wifiReceiver;
 	private WifiManager mainWifi;
-	private List<ScanResult> wifiList = null;
-	
+	private List<ScanResult> wifiList = null;	
 	private Handler handler = new Handler();
-	
-	private String APList = "";
-	private SensorValue sAPList;
 	
 	private List<WifiDevice> scannedDevices;
 	private SensorValue scannedDevicesSV;
 	
 	private int defaultSize = 5;
+	private int totalSize;
 	private int scan_interval = 10; // sec	
 
 	public WifiSensor(Context context) {
 		super(context);
 		name = "Wifi Scan Sensor";
 		
-		sAPList = new SensorValue(SensorValue.UNIT.STRING, SensorValue.TYPE.OTHER);
-		
 		scannedDevicesSV = new SensorValue(SensorValue.UNIT.LIST, SensorValue.TYPE.OTHER);
-		scannedDevicesSV.setValue(new LinkedList<WifiDevice>());
-		
+		scannedDevicesSV.setValue(new LinkedList<WifiDevice>());		
 		scannedDevices = new LinkedList<WifiDevice>();
 	}
 	
@@ -61,9 +54,11 @@ public class WifiSensor extends AbstractSensor {
 		wifiReceiver = new BroadcastReceiver() {
 			public void onReceive(Context context, Intent intent) {
 				context.unregisterReceiver(this);
-				APList = "";
+				scannedDevices.clear();
+
 				wifiList = mainWifi.getScanResults();
-				int length = Math.min(wifiList.size(), defaultSize);
+				totalSize = wifiList.size();
+				int length = Math.min(totalSize, defaultSize);
 				
 		        for(int i = 0; i < length; i++){
 		        	ScanResult result = wifiList.get(i);
@@ -72,16 +67,14 @@ public class WifiSensor extends AbstractSensor {
 		        			result.SSID, result.BSSID, result.level, result.capabilities, 
 		        			(float)result.frequency/1000);
 		        	SensorRegistry.getInstance().log("WiFi", message);
-		        	APList += message + (i == (length-1) ? "" : "\n\n");
 		        }
 		        
 		        for(int i = 0; i < wifiList.size(); i++){
 		        	ScanResult result = wifiList.get(i);
-		        	scannedDevices.add(new WifiDevice(result.SSID, result.BSSID, result.capabilities, 
+		        	scannedDevices.add(new WifiDevice(i+1, result.SSID, result.BSSID, result.capabilities, 
 		        			result.level, (float)result.frequency/1000));
 		        }
 		        scannedDevicesSV.setValue(scannedDevices);
-		        sAPList.setValue(APList);
 		        notifyListeners();
 			}
 		};			
@@ -94,6 +87,7 @@ public class WifiSensor extends AbstractSensor {
 	}
 	
 	public class WifiDevice{
+		private SensorValue DeviceID = new SensorValue(SensorValue.UNIT.NUMBER, SensorValue.TYPE.OTHER);
 		private SensorValue SSID = new SensorValue(SensorValue.UNIT.STRING, SensorValue.TYPE.DEVICE_NAME);
 		private SensorValue BSSID = new SensorValue(SensorValue.UNIT.STRING, SensorValue.TYPE.MAC_ADDRESS);
 		private SensorValue capabilities = new SensorValue(SensorValue.UNIT.STRING, SensorValue.TYPE.OTHER);
@@ -103,15 +97,20 @@ public class WifiSensor extends AbstractSensor {
 		public WifiDevice() {
 		}
 		
-		public WifiDevice(String name, String MAC, String cap, int rssi, float freq){
+		public WifiDevice(int id, String name, String MAC, String cap, int rssi, float freq){
 			if (name != null)
 				this.SSID.setValue(name);
 			if (MAC != null)
 				this.BSSID.setValue(MAC);
 			if (cap != null)
 				this.capabilities.setValue(cap);
+			this.DeviceID.setValue(id);
 			this.frequency.setValue(freq);
 			this.RSSI.setValue(rssi);
+		}
+		
+		public Object getID(){
+			return DeviceID.getValue();
 		}
 		
 		public String getSSID(){
@@ -136,7 +135,10 @@ public class WifiSensor extends AbstractSensor {
 		
 		@Override
 		public String toString() {
-			return SSID.getValueRepresentation() +"; " + BSSID.getValueRepresentation() + "; " + frequency.getValueRepresentation() + "; " + RSSI.getValueRepresentation() + "; " + capabilities.getValueRepresentation();
+			return DeviceID.getValue() + ". " + SSID.getValueRepresentation() + "; BSSID: " + 
+					BSSID.getValueRepresentation() + "; freq: " + frequency.getValueRepresentation() + 
+					" GHz; RSS: " + RSSI.getValueRepresentation() + " dBm; " + capabilities.getValueRepresentation()
+					+ (Integer.parseInt(DeviceID.getValue().toString()) == (totalSize) ? "" : '\n');
 		}
 	}
 }
