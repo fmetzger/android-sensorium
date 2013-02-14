0eded34b7e4bc4d656800a939702264c41e3b395
Florian Metzger <florian.metzger@univie.ac.at>
2013-02-14 15:19:21 +0100

    initial upload dialogpreference

--- Sensorium/src/at/univie/sensorium/logging/HTTPSUploaderDialogPreference.java
+++ Sensorium/src/at/univie/sensorium/logging/HTTPSUploaderDialogPreference.java
@@ -3,6 +3,9 @@ package at.univie.sensorium.logging;
 import android.content.Context;
 import android.preference.DialogPreference;
 import android.util.AttributeSet;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.Toast;
 import at.univie.sensorium.R;
 import at.univie.sensorium.SensorRegistry;
@@ -13,14 +16,29 @@ public class HTTPSUploaderDialogPreference extends DialogPreference {
 		super(context, attrs);
 		setDialogLayoutResource(R.layout.upload_dialogpreference);
 		setTitle("Upload Data");
-		
 	}
 	
 	@Override
-	protected void onClick() {
-		Toast x = Toast.makeText(getContext(), "Starting upload...", Toast.LENGTH_SHORT);
-		x.show();
-		SensorRegistry.getInstance().getJSONLogger().upload();
+	protected void onBindDialogView(View view) {
+		super.onBindDialogView(view);
+		
+		Button button = (Button) view.findViewById(R.id.uploadpref_button);
+		button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast x = Toast.makeText(getContext(), "Starting upload...", Toast.LENGTH_SHORT);
+				x.show();
+				SensorRegistry.getInstance().getJSONLogger().upload();
+			}
+		});
 	}
+	
+//	@Override
+//	protected void onClick() {
+//		Toast x = Toast.makeText(getContext(), "Starting upload...", Toast.LENGTH_SHORT);
+//		x.show();
+//		SensorRegistry.getInstance().getJSONLogger().upload();
+//	}
 
 }
